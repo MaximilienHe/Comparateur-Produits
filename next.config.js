9168cbf49ae87d74b12d39b3114c021f31c803bb
Maximilien Herr <herrmaximilien@gmail.com>
2023-06-02 09:10:44 +0200

    Base URL for all app

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
-
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
   // Ajouter la configuration du module dotenv ici
   env: {
     DB_HOST: process.env.DB_HOST,
