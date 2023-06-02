72bdfe28d5961ff869e9582bd1319a53af89bd1b
Maximilien Herr <herrmaximilien@gmail.com>
2023-06-02 14:58:48 +0200

    Fix base path and prefix

--- next.config.js
+++ next.config.js
@@ -4,8 +4,9 @@ require('dotenv').config()
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
-  // Ajouter la configuration du module dotenv ici
+  basePath: '/comparateur',
+  assetPrefix: '/comparateur/', 
+   // Ajouter la configuration du module dotenv ici
   env: {
     DB_HOST: process.env.DB_HOST,
     DB_USER: process.env.DB_USER,
