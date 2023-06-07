2d708a1ebfd4a72c4dc9579b786ea5fdf9309049
Maximilien Herr <herrmaximilien@gmail.com>
2023-06-07 16:09:31 +0200

    Add log of auth

--- utils/auth.js
+++ utils/auth.js
@@ -6,12 +6,14 @@ export async function validateApiKeyAndPermissions(apiKey, method) {
     const result = await db.query(sql, apiKey)
 
     if (result.length === 0) {
+      console.log('Invalid API key')
       return { isValid: false, message: 'Invalid API key' }
     }
 
     const keyData = result[0]
 
     if (!keyData.isActive) {
+      console.log('API key is not active')
       return { isValid: false, message: 'API key is not active' }
     }
 
@@ -25,11 +27,13 @@ export async function validateApiKeyAndPermissions(apiKey, method) {
     const hasPermission = methodToPermission[method]
 
     if (!hasPermission) {
+      console.log(`API key does not have permission to perform ${method} requests`)
       return {
         isValid: false,
         message: `API key does not have permission to perform ${method} requests`,
       }
     }
+    console.log('API key is valid')
     return { isValid: true }
   } catch (error) {
     console.error(error)
