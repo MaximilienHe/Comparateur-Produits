e8dca9d0b10a37a3102b5c7deeb43893fd977308
MaximilienHe <maximilienherr@gmail.com>
2023-11-01 16:40:28 +0100

    Final fix images

--- components/Spinner.js
+++ components/Spinner.js
@@ -7,7 +7,7 @@ const Spinner = () => {
     <div className={styles.spinnerContainer}>
       <div className={styles.spinner}>
         <Image
-          src="./public/logoDroidSoft.png"
+          src="/logoDroidSoft.png"
           alt="Loading..."
           width={52}
           height={35}
